df50aec66aea9006121e83e05f2d3e11b36906f1
Raphael Cabral <brcmesquita@gmail.com>
2019-11-18 17:47:01 -0300

    Add button to "mark as undone"

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $itemsQuery = $db->prepare("
   WHERE user = :user
   ");
 
-$itemsQuery-> execute([
+$itemsQuery->execute([
   'user' => $_SESSION['user_id']
 ]);
 
@@ -19,8 +19,8 @@ $items = $itemsQuery->rowCount() ? $itemsQuery : [];
 #}
 ?>
 
-<!DOCTYPE html >
-<html lang="pt-br" >
+<!DOCTYPE html>
+<html lang="pt-br">
 
 <head>
   <meta charset="UTF-8" />
@@ -32,24 +32,25 @@ $items = $itemsQuery->rowCount() ? $itemsQuery : [];
   <div class="list">
     <h1 class="header">To do.</h1>
 
-    <?php if(!empty($items)): ?>
-    <ul class="items">
-      <?php foreach($items as $item): ?>
-      <li>
-        <span class="item<?php echo $item['done'] ? ' done' : '' ?>"><?php echo $item['name']; ?></span>
-        <?php if(!$item['done']): ?>
-        <a href="#" class="done-button">Mark as done</a>
-        <?php endif; ?>
-      </li>
-      <?php endforeach; ?>
-    </ul>
-    <?php else: ?>
+    <?php if (!empty($items)) : ?>
+      <ul class="items">
+        <?php foreach ($items as $item) : ?>
+          <li>
+            <span class="item<?php echo $item['done'] ? ' done' : '' ?>"><?php echo $item['name']; ?></span>
+            <?php if (!$item['done']) : ?>
+              <a href="mark.php?as=done&item=<?php echo $item['id']; ?>" class="done-button">Mark as done</a>
+            <?php else : ?>
+              <a href="mark.php?as=notdone&item=<?php echo $item['id']; ?>" class="done-button">Mark as undone</a>
+            <?php endif; ?>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    <?php else : ?>
       <p>Você não adicionou nenhuma tarefa ainda</p>
     <?php endif; ?>
 
     <form class="item-add" action="add.php" method="post">
-      <input type="text" name="name" id="name" placeholder="Digite uma tarefa aqui." class="input" autocomplete="off"
-        required />
+      <input type="text" name="name" id="name" placeholder="Digite uma tarefa aqui." class="input" autocomplete="off" required />
       <input type="submit" value="Add" class="submit" />
     </form>
   </div>
