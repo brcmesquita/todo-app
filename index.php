ad24be9994f75215b4c711e21f8436b417c4e0d9
Raphael Cabral <brcmesquita@gmail.com>
2019-11-18 17:21:52 -0300

    Back and Front-End Done

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
+
+require_once 'app/init.php';
+
+$itemsQuery = $db->prepare("
+  SELECT id, name, done
+  FROM items
+  WHERE user = :user
+  ");
+
+$itemsQuery-> execute([
+  'user' => $_SESSION['user_id']
+]);
+
+$items = $itemsQuery->rowCount() ? $itemsQuery : [];
+
+#foreach($items as $item) {
+#  print_r($item);
+#}
+?>
+
 <!DOCTYPE html >
 <html lang="pt-br" >
 
@@ -11,14 +32,20 @@
   <div class="list">
     <h1 class="header">To do.</h1>
 
+    <?php if(!empty($items)): ?>
     <ul class="items">
+      <?php foreach($items as $item): ?>
       <li>
-        <span class="item">Pick up shopping.</span>
+        <span class="item<?php echo $item['done'] ? ' done' : '' ?>"><?php echo $item['name']; ?></span>
+        <?php if(!$item['done']): ?>
         <a href="#" class="done-button">Mark as done</a>
+        <?php endif; ?>
       </li>
-      <li><span class="item done">Learn PHP.</span></li>
-      <li><span class="item">Go vegan.</span></li>
+      <?php endforeach; ?>
     </ul>
+    <?php else: ?>
+      <p>Você não adicionou nenhuma tarefa ainda</p>
+    <?php endif; ?>
 
     <form class="item-add" action="add.php" method="post">
       <input type="text" name="name" id="name" placeholder="Digite uma tarefa aqui." class="input" autocomplete="off"
