b0642fc78ddcd44346bf0a2a98f763db7d3fe0af
Raphael Cabral <brcmesquita@gmail.com>
2020-06-13 11:46:21 -0300

    update conection string

--- app/init.php
+++ app/init.php
@@ -1,10 +1,17 @@
 <?php
 
+$hostname = "localhost";
+
+$username = "root";
+$password = "";
+
+$database = "todo";
+
 session_start();
 
 $_SESSION['user_id'] = 1;
 
-$db = new PDO('mysql:dbname=todo;host=localhost', 'root', 'root');
+$db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
 
 // Handle this in some other way
 if(!isset($_SESSION['user_id'])) {
